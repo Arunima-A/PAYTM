35583b2a7bcac4a7aeec9c6df5029fcb5f02a1b5
Arunima-A <155307341+Arunima-A@users.noreply.github.com>
2024-06-27 04:15:15 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://arunima2:Aru%400412@cluster0.f7hy09r.mongodb.net/paytm")
+mongoose.connect("your-mongodb-url")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
